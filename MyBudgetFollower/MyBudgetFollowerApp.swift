0950ac8cc40da6e3054e0107b4ac96a6a6388438
antho68 <baumleanthony@gmail.com>
2024-09-30 07:49:34 +0200

    Initial Commit

---
+++ MyBudgetFollower/MyBudgetFollowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBudgetFollowerApp.swift
+//  MyBudgetFollower
+//
+//  Created by Anthony Baumle on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyBudgetFollowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBudgetFollowerTests/MyBudgetFollowerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyBudgetFollowerTests.swift
+//  MyBudgetFollowerTests
+//
+//  Created by Anthony Baumle on 30/09/2024.
+//
+
+import Testing
+
+struct MyBudgetFollowerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
